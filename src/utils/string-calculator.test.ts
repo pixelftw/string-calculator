a4fdd3748bc935546a74c4ac369d88e765be8116
Vikas Yadav <geekyvikas240052@gmail.com>
2025-08-20 15:31:14 +0530

    feat(string-calculator): implement custom delimeter and negative number handling to setisfy test cases

--- src/utils/string-calculator.test.ts
+++ src/utils/string-calculator.test.ts
@@ -14,17 +14,40 @@ describe("String calculator utility", () => {
     expect(output).toBe(5);
   });
 
+  it("should ignore whitespace and return the number", () => {
+    const output = add(" 5 ");
+
+    expect(output).toBe(5);
+  });
+
   it("should add two comma separated numbers", () => {
     const output = add("4,5");
 
     expect(output).toBe(9);
   });
 
-  it.todo("should handle new lines");
+  it("should ignore whitespace and still perform comma separated sum", () => {
+    const output = add(" 5 , 4");
 
-  it.todo("should support different delimiters");
+    expect(output).toBe(9);
+  });
 
-  it.todo("should throw all negative numbers");
+  it("should handle new lines", () => {
+    const output = add("4,5\n,6");
+    expect(output).toBe(15);
+  });
 
-  it.todo("should throw and show error message with multiple negative numbers");
+  it("should support different delimiters", () => {
+    const output = add("//;\n1;2;4;\n2");
+
+    expect(output).toBe(9);
+  });
+
+  it("should throw negative numbers", () => {
+    expect(() => add("1,-3")).toThrow("negative numbers not allowed -3");
+  });
+
+  it("should throw and show error message with multiple negative numbers", () => {
+    expect(() => add("1,-3,-4")).toThrow("negative numbers not allowed -3,-4");
+  });
 });
--- src/utils/string-calculator.ts
+++ src/utils/string-calculator.ts
@@ -1,22 +1,57 @@
-function addCommaSeperatedNumbersFromString(string: string) {
-  const numbers = string.trim().split(",");
-  let result = 0;
+const COMMA_AND_NEW_LINE: RegExp = /[,\n]+/;
 
-  numbers.forEach((value) => {
-    result = result + Number(value);
-  });
+function addNumbers(string: string, delimiter: RegExp) {
+  const numbers = string.trim().split(delimiter);
+  const negativeNumbers: number[] = [];
+
+  const result = numbers.reduce((acc, curr) => {
+    const number = Number(curr);
+
+    if (number < 0) negativeNumbers.push(number);
+
+    return acc + Number(number);
+  }, 0);
+
+  if (negativeNumbers.length) {
+    throw new Error(
+      `negative numbers not allowed ${negativeNumbers.join(",")}`
+    );
+  }
 
   return result;
 }
 
+interface ParsedDelimiter {
+  parsedValue: string;
+  delimiter: RegExp;
+}
+
+function parseDelimiter(value: string): ParsedDelimiter {
+  let delimiter = COMMA_AND_NEW_LINE;
+  let parsedValue = value;
+  const delimiterEndIndex = value.indexOf("\n");
+
+  if (value.startsWith("//")) {
+    delimiter = new RegExp(value.substring(2, delimiterEndIndex));
+    parsedValue = value.substring(delimiterEndIndex + 1);
+  }
+
+  return {
+    delimiter,
+    parsedValue,
+  };
+}
+
 export function add(stringValue: string) {
-  if (!stringValue) {
+  if (!stringValue || stringValue.trim() === "") {
     return 0;
   }
 
-  if (stringValue.indexOf(",")) {
-    return addCommaSeperatedNumbersFromString(stringValue);
+  if (stringValue.length === 1) {
+    return Number(stringValue);
   }
 
-  return Number(stringValue);
+  const { delimiter, parsedValue } = parseDelimiter(stringValue);
+
+  return addNumbers(parsedValue, delimiter);
 }
