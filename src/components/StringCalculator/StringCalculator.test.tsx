c16d5faebce7dec18b82116c9b0a4826db9e3cb8
Vikas Yadav <geekyvikas240052@gmail.com>
2025-08-20 23:41:35 +0530

    feat: add test cases for StringCalculator component

---
+++ src/components/StringCalculator/StringCalculator.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it } from "vitest";
+
+describe("StringCalculator", () => {
+  it.todo("renders app title as String Calculator inside h1 tag");
+
+  it.todo("submits the input and renders the result when click on the button");
+
+  it.todo("submits the input and renders the result when click enter key");
+
+  it.todo("displays the error in case of negative number");
+
+  it.todo("removes the result and error in case user inputs anything");
+});
